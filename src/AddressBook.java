b1af398829cd036acf0f3806624361b3ed837dc6
bhushan-r-mahajan <bhushanmahajan1997@gmail.com>
2021-05-08 13:50:53 +0530

    All CURD operations for multiple addressbooks are performed in this file

---
+++ src/AddressBook.java
@@ -0,0 +1,72 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class AddressBook {
+    ArrayList<MultipleAddressBook> AddressBookName = new ArrayList<>();
+    Scanner input = new Scanner(System.in);
+    int bookNumber = 0;
+    String name;
+
+    public static void main(String[] args) {
+        AddressBook menu = new AddressBook();
+        menu.menu();
+    }
+
+    public void multipleAddressBook() {
+        System.out.print("Enter the name of Address Book: ");
+        name = input.nextLine();
+        AddressBookName.add(new MultipleAddressBook(name));
+        System.out.println("New Address Book Created with name: " + name);
+    }
+
+    public void SelectAddressBook() {
+        System.out.println("You are Currently in " + AddressBookName.get(bookNumber) + " AddressBook");
+        if (AddressBookName.size() > 0) {
+
+            for (int i = 0; i < AddressBookName.size(); i++)
+                System.out.println(i + " - "+ AddressBookName.get(i));
+
+            System.out.print("Pick Address Book Number = ");
+            bookNumber = Integer.parseInt(input.next());
+        }
+    }
+
+    public void menu() {
+        Scanner input = new Scanner(System.in);
+        multipleAddressBook();
+        while (true) {
+            System.out.println("<---- Address Book ---->");
+            System.out.println("1. Add Contact.");
+            System.out.println("2. Edit Contact.");
+            System.out.println("3. Delete Contact.");
+            System.out.println("4. Add Another Address Book: ");
+            System.out.println("5. See Contacts: ");
+            System.out.println("6. Search Contacts with Same State/City: ");
+            System.out.println("7. Exit");
+
+            int choice = input.nextInt();
+            switch (choice) {
+                case 1: {
+                    SelectAddressBook();
+                    AddressBookName.get(bookNumber).addContact();
+                    break;
+                }
+                case 2: AddressBookName.get(bookNumber).editContact();
+                    break;
+                case 3: AddressBookName.get(bookNumber).deleteContact();
+                    break;
+                case 4: multipleAddressBook();
+                    break;
+                case 5: {
+                    System.out.println("<---- These are the AddressBooks Present ---->");
+                    SelectAddressBook();
+                    AddressBookName.get(bookNumber).printContacts();
+                    break;
+                }
+                case 6: AddressBookName.get(bookNumber).searchContact();
+                    break;
+                case 7: System.exit(0);
+            }
+        }
+    }
+}
