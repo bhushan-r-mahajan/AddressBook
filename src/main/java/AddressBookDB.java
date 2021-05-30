b0b89e4cb9ee0108a670e4e855eefd2e3b7b143e
bhushan-r-mahajan <bhushanmahajan1997@gmail.com>
2021-05-30 14:28:14 +0530

    Implemented search according to date

--- src/main/java/AddressBookDB.java
+++ src/main/java/AddressBookDB.java
@@ -1,4 +1,5 @@
 import java.sql.*;
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -18,23 +19,53 @@ public class AddressBookDB {
     }
 
     public List<Data> getAllDetailsFromTable() throws CustomException {
-        List<Data> contactDetails = new ArrayList<>();
         String sql = "select * from contact_details;";
+        return this.getDataFromDBWhenSQLGiven(sql);
+    }
+
+    private List<Data> getResultSet(ResultSet resultSet) throws CustomException {
+        List<Data> contactDetails = new ArrayList<>();
+        try {
+            while (resultSet.next()) {
+            int id = resultSet.getInt("id");
+            String firstName = resultSet.getString("first_name");
+            String lastName = resultSet.getString("last_name");
+            String address = resultSet.getString("address");
+            String city = resultSet.getString("city");
+            String state = resultSet.getString("state");
+            int zipp = resultSet.getInt("zip");
+            String phone = resultSet.getString("phone_number");
+            String email = resultSet.getString("email");
+            contactDetails.add(new Data(id, firstName, lastName, address, city, state, zipp, phone, email));
+        }
+    } catch (SQLException e) {
+        throw new CustomException("Query Failed!!");
+    }
+        return contactDetails;
+    }
+
+    public int updateContactDetailsInDB(String firstName, String phone) throws CustomException {
+        String sql = String.format("update contact_details set phone_number = '%s' where first_name = '%s';", phone, firstName);
+        try (Connection connection = this.getConnection()) {
+            Statement statement = connection.createStatement();
+            return statement.executeUpdate(sql);
+        } catch (SQLException e) {
+            throw new CustomException("Update Failed!!!");
+        }
+    }
+
+    public List<Data> getContactDetailsAccordingToDate(LocalDate from, LocalDate to) throws CustomException {
+        String sql = String.format("select * from contact_details where add_date between '%s' and '%s'",
+                Date.valueOf(from), Date.valueOf(to));
+        return this.getDataFromDBWhenSQLGiven(sql);
+    }
+
+    private List<Data> getDataFromDBWhenSQLGiven(String sql) throws CustomException {
+        List<Data> contactDetails;
         try (Connection connection = this.getConnection()) {
             Statement statement = connection.createStatement();
             ResultSet resultSet = statement.executeQuery(sql);
-            while (resultSet.next()) {
-                int id = resultSet.getInt("id");
-                String firstName = resultSet.getString("first_name");
-                String lastName = resultSet.getString("last_name");
-                String address = resultSet.getString("address");
-                String city = resultSet.getString("city");
-                String state = resultSet.getString("state");
-                int zipp = resultSet.getInt("zip");
-                String phone = resultSet.getString("phone_number");
-                String email = resultSet.getString("email");
-                contactDetails.add(new Data(id, firstName, lastName, address, city, state, zipp, phone, email));
-            }
+            contactDetails = getResultSet(resultSet);
         } catch (SQLException e) {
             throw new CustomException("Query Failed!!");
         }
--- src/test/java/AddressBookDBTest.java
+++ src/test/java/AddressBookDBTest.java
@@ -1,5 +1,7 @@
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
 import java.util.List;
 
 public class AddressBookDBTest {
@@ -10,6 +12,23 @@ public class AddressBookDBTest {
         AddressBookDB addressBookDB = new AddressBookDB();
         contactDetails = addressBookDB.getAllDetailsFromTable();
         System.out.println("The Number of Entries in DataBase are = " + contactDetails.size());
+        Assertions.assertEquals(3, contactDetails.size());
+    }
+
+    @Test
+    void givenNewSalary_WhenUpdated_ShouldPassTestAndBeInSync() throws CustomException {
+        AddressBookDB addressBookDB = new AddressBookDB();
+        int result = addressBookDB.updateContactDetailsInDB("amol", "2222222222");
+        Assertions.assertEquals(1, result);
+    }
+
+    @Test
+    void givenDate_WhenGiven_ShouldRetrieveEmployeeData() throws CustomException {
+        AddressBookDB addressBookDB = new AddressBookDB();
+        List<Data> contactDetails;
+        LocalDate from = LocalDate.of(2020, 01, 01);
+        LocalDate to = LocalDate.now();
+        contactDetails = addressBookDB.getContactDetailsAccordingToDate(from, to);
         Assertions.assertEquals(3, contactDetails.size());
     }
 }
